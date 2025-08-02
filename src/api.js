f9a1cb4176492f560436d627a22658bf1ad2445a
Adedayo Williams <adedayo.williams09@gmail.com>
2025-08-02 13:39:50 +0100

    update api.js

--- src/api.js
+++ src/api.js
@@ -5,6 +5,7 @@ import axios from 'axios';
 // Set baseURL based on environment
 const baseURL = import.meta.env.VITE_API_BASE_URL || 'https://swiftcart-server-1i80.onrender.com/api';
 
+
 const api = axios.create({
   baseURL,
   withCredentials: true, // send cookies
